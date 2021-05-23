23d4dd0096c460849695955ce9b83737616cb349
erebuxy <erebuxy@gmail.com>
2021-05-23 03:35:17 +0100

    Impl record sig

--- src/ppx_make_utils.ml
+++ src/ppx_make_utils.ml
@@ -1,14 +1,62 @@
 module P = Ppxlib
 module Ast_helper = Ppxlib.Ast_helper
 
-let unsupported_error ~loc =
-  P.Location.raise_errorf ~loc "ppx_make cannot derive this type"
+let unsupported_error P.{ txt; loc } =
+  P.Location.raise_errorf ~loc "type %s cannot be derived" txt
 
 let make_type_decl_generator f =
   P.Deriving.Generator.V2.make_noarg @@ fun ~ctxt (rec_flag, tds) ->
   let loc = P.Expansion_context.Deriver.derived_item_loc ctxt in
   tds |> List.map @@ f ~loc rec_flag
 
-let get_type_name (type_decl : P.type_declaration) = type_decl.ptype_name.txt
+let gen_make_name P.{ txt = name; loc } = P.{ txt = "make_" ^ name; loc }
 
-let gen_make_name ~loc name = P.{ txt = "make_" ^ name; loc }
+(* Core Type Utils *)
+
+let unit_core_type ~loc =
+  Ast_helper.Typ.constr ~loc P.{ txt = P.Lident "unit"; loc } []
+
+let core_type_of_name P.{ txt = name; loc } =
+  Ast_helper.Typ.constr ~loc P.{ txt = P.Lident name; loc } []
+
+let is_core_type_optional (ct : P.core_type) =
+  match ct.ptyp_desc with
+  | Ptyp_constr ({ txt = Lident "option"; _ }, _)
+  | Ptyp_constr ({ txt = Lident "list"; _ }, _)
+  | Ptyp_constr ({ txt = Lident "string"; _ }, []) ->
+      true
+  | _ -> false
+
+let strip_option (ct : P.core_type) =
+  match ct.ptyp_desc with
+  | Ptyp_constr ({ txt = Lident "option"; _ }, [ in_ct ]) -> in_ct
+  | _ -> ct
+
+(* Attributes Utils *)
+
+type attr_type = No_attr | Main | Required | Default of P.expression
+
+let get_attributes (attrs : P.attribute list) =
+  let check_res ~loc acc cur =
+    match (acc, cur) with
+    | No_attr, _ -> cur
+    | _, No_attr -> acc
+    | _, _ ->
+        P.Location.raise_errorf ~loc
+          "field cannot have more than one attributes"
+  in
+  List.fold_left
+    (fun acc (attr : P.attribute) ->
+      (match attr.attr_name.txt with
+      | "main" -> Main
+      | "required" -> Required
+      | "default" -> (
+          match attr.attr_payload with
+          | PStr [ { pstr_desc = Pstr_eval (expr, _); _ } ] -> Default expr
+          | _ ->
+              P.Location.raise_errorf ~loc:attr.attr_loc
+                "value in default attribute is not supported")
+      (* ignore unknown attrs *)
+      | _ -> No_attr)
+      |> check_res ~loc:attr.attr_loc acc)
+    No_attr attrs
