10e2d34d308192d15d3116126b4bf29a0a7c6722
Boning <me@boni.ng>
2021-05-26 01:34:28 +0100

    Impl tuple

--- test/variant_types.ml
+++ test/variant_types.ml
@@ -17,33 +17,4 @@ type record_v =
       cm2 : int option;
       cm3 : int option; [@main]
     }
-[@@deriving_inline make]
-
-let _ = fun (_ : record_v) -> ()
-
-let (make_r_basic_of_record_v : b1:int -> unit -> record_v) =
- fun ~b1 _ -> R_basic { b1 }
-
-let _ = make_r_basic_of_record_v
-
-let (make_r_complex_of_record_v :
-      c1:int ->
-      ?c2:int ->
-      ?c3:int list ->
-      ?c4:string ->
-      ?c5:int ->
-      c6:string ->
-      unit ->
-      record_v) =
- fun ~c1 ?c2 ?(c3 = []) ?(c4 = "") ?(c5 = 1024) ~c6 _ ->
-  R_complex { c1; c2; c3; c4; c5; c6 }
-
-let _ = make_r_complex_of_record_v
-
-let (make_r_complex_with_main_of_record_v :
-      ?cm2:int -> int -> int option -> record_v) =
- fun ?cm2 cm1 cm3 -> R_complex_with_main { cm1; cm2; cm3 }
-
-let _ = make_r_complex_with_main_of_record_v
-
-[@@@end]
+[@@deriving make]
